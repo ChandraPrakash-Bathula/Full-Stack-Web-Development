89eee21e260492bff05dc43ce043cf69641c9182
ChandraPrakash-Bathula <chandu.bathula16@gmail>
2024-01-09 18:05:08 -0600

    console and comments in JS

--- Javascript/intro.js
+++ Javascript/intro.js
@@ -1,1 +1,17 @@
-console.log("Hello World");
+// Introduction to Javascript : ES6
+
+// Console Statement: Console.log statement allows you to see the results in console.
+console.log("Hello World from Javascript");
+
+// Comments in Javascript:
+
+var number = 5; // This is an inline comment;
+
+
+/* This is a 
+
+Multi-line 
+
+Comment
+
+in javascript */
