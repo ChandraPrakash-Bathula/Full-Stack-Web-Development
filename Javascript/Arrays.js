b4995f30af6458bae4215e7fdc061c22eee33bd8
ChandraPrakash-Bathula <chandu.bathula16@gmail>
2024-01-10 17:42:42 -0600

    arrays in Javascript

---
+++ Javascript/Arrays.js
@@ -0,0 +1,11 @@
+/* Arrays: Store multiple values with arrays.
+It starts with [ and ends with ] we can have any no.of elements and elements can be of any type of datatypes. */
+var array = ["John", 4, "wick", "Bro"];
+console.log(array);
+
+/* Nested Arrays: An array in another is called a nested array or a multi-dimensional array. */
+
+var ourArray = [  ["John", "wick", 4, "Bro"], ["everything", 101019010],"kinguu"];
+console.log(ourArray);
+
+console.log(ourArray[1][0]);
