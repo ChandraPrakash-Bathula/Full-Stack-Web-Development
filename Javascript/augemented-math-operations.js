024b0c81c89a8291deb9a8f78a750b4c92b5f4d7
ChandraPrakash-Bathula <chandu.bathula16@gmail>
2024-01-10 15:18:19 -0600

    augemented Math operations in Javascript

---
+++ Javascript/augemented-math-operations.js
@@ -0,0 +1,32 @@
+var c = 9;
+var b = 12;
+var a = 11;
+
+/* now we can even add the numbers like this: */
+
+c += 12; // same as c = c + 12
+b += 12; // same as b = b + 12
+a += 12; // same as a = a + 12
+console.log(c, b, a);
+
+/* This is same for the subraction too: */
+
+c -= 12; //same as c = c - 12
+b -= 12; //same as b = b - 12
+a -= 12; //same as a = a - 12
+
+console.log(c, b, a);
+
+/* Even Multiplication and Division can be done this way: */
+
+c *= 12; // same as c = c * 12
+b *= 12; // same as b = b * 12
+a *= 12; // same as a = a * 12
+
+console.log(c, b, a);
+
+c /= 12; // same as c = c / 12
+b /= 12; // same as b = b / 12
+a /= 12; // same as a = a / 12
+
+console.log(c, b, a);
