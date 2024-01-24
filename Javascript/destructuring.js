3308e823e07bce631c45d9694e6a3e00408e5d85
ChandraPrakash-Bathula <chandu.bathula16@gmail>
2024-01-24 16:19:24 -0600

    some functions

--- Javascript/destructuring.js
+++ Javascript/destructuring.js
@@ -59,16 +59,81 @@ console.log(stats);
 console.log(half(stats));
 
 const statis = {
-  max: 54.18,
-  std: 4.5,
-  median: 27,
-  min: -0.72,
-  avg: 45,
+    max: 54.18,
+    std:4.5,
+    median:27,
+    min:-0.72,
+    avg:45
 };
-const halfes = (function () {
-  return function halfes(statis) {
-    return (statis.max + statis.min) / 1.8;
-  };
+const halfes = (function(){
+    return function halfes({max,min}){
+        return (max + min)/2.0;
+    };
 })();
 console.log(statis);
 console.log(halfes(statis));
+ /* This will be commonly used when we do the API calls. When you are getting information from an AJAX request or an API request. It will have a lot of information than we need. And we can use destructuring to get it down what we actually want to work with. */
+
+
+/* Create Strings using Template Literals. */
+
+const person = {
+    name: 'Harvey Spectre',
+    age: 24
+};
+
+//Template Literal with multi-line and string interpolation.
+
+const greeting = `Hello, Mike, I am ${person.name}! I am the best closer in the city for more than ${person.age} years.`
+
+console.log(greeting);
+
+const result = {
+    success: ['max-length', 'no-amd', 'prefer-arrow-functions'],
+    failure: ['no-var', 'var-on-top', 'linebreak'],
+    skipped: ['id-blacklist', 'no-dup-keys']
+};
+function makeList(arr) {
+    const resultDisplayArray = [];
+    for (let i = 0; i < arr.length; i++) {
+        resultDisplayArray.push(`<li class='text-warning'>${arr[i]}</li>`)
+    }
+    return resultDisplayArray;
+};
+const resultDisplayArray = makeList(result.success);
+console.log(resultDisplayArray);
+
+const any = {
+    one: ['MB', 'PK', 'AA'],
+    two: ['RC', 'NTR', 'PB'],
+    three: ['SRK', 'Bhai', 'Ameer']
+};
+
+function something(arra) {
+    const displayingArra = [];
+    for (var i = 0; i < arra.length; i++) {
+        displayingArra.push(`<li class="text-no-warning">${arra[i]}</li>`);
+    }
+    return displayingArra;
+}
+const displayingArra = something(any.one);
+console.log(displayingArra);
+
+/* Write Concise Object Literal Declarations Using Simple Fields. */
+
+const createPerson = (name,age,gender) => {
+    return {
+        name:name,
+        age:age,
+        gender:gender
+    };
+};
+console.log(createPerson('Harvey Spectre',24,'Alpha-Male'));
+
+const creatingSomething = (name,age) => {
+    return {
+        name:name,
+        age:age
+    };
+};
+console.log(creatingSomething('Mike',28));
