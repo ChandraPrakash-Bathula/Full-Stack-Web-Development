6b28097df7ab2b6a2ab1ebe41341c7df4f088b41
ChandraPrakash-Bathula <chandu.bathula16@gmail>
2024-01-24 16:40:15 -0600

    destructuring and classes

--- Javascript/destructuring.js
+++ Javascript/destructuring.js
@@ -59,81 +59,125 @@ console.log(stats);
 console.log(half(stats));
 
 const statis = {
-    max: 54.18,
-    std:4.5,
-    median:27,
-    min:-0.72,
-    avg:45
+  max: 54.18,
+  std: 4.5,
+  median: 27,
+  min: -0.72,
+  avg: 45,
 };
-const halfes = (function(){
-    return function halfes({max,min}){
-        return (max + min)/2.0;
-    };
+const halfes = (function () {
+  return function halfes({ max, min }) {
+    return (max + min) / 2.0;
+  };
 })();
 console.log(statis);
 console.log(halfes(statis));
- /* This will be commonly used when we do the API calls. When you are getting information from an AJAX request or an API request. It will have a lot of information than we need. And we can use destructuring to get it down what we actually want to work with. */
-
+/* This will be commonly used when we do the API calls. When you are getting information from an AJAX request or an API request. It will have a lot of information than we need. And we can use destructuring to get it down what we actually want to work with. */
 
 /* Create Strings using Template Literals. */
 
 const person = {
-    name: 'Harvey Spectre',
-    age: 24
+  name: "Harvey Spectre",
+  age: 24,
 };
 
 //Template Literal with multi-line and string interpolation.
 
-const greeting = `Hello, Mike, I am ${person.name}! I am the best closer in the city for more than ${person.age} years.`
+const greeting = `Hello, Mike, I am ${person.name}! I am the best closer in the city for more than ${person.age} years.`;
 
 console.log(greeting);
 
 const result = {
-    success: ['max-length', 'no-amd', 'prefer-arrow-functions'],
-    failure: ['no-var', 'var-on-top', 'linebreak'],
-    skipped: ['id-blacklist', 'no-dup-keys']
+  success: ["max-length", "no-amd", "prefer-arrow-functions"],
+  failure: ["no-var", "var-on-top", "linebreak"],
+  skipped: ["id-blacklist", "no-dup-keys"],
 };
 function makeList(arr) {
-    const resultDisplayArray = [];
-    for (let i = 0; i < arr.length; i++) {
-        resultDisplayArray.push(`<li class='text-warning'>${arr[i]}</li>`)
-    }
-    return resultDisplayArray;
-};
+  const resultDisplayArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    resultDisplayArray.push(`<li class='text-warning'>${arr[i]}</li>`);
+  }
+  return resultDisplayArray;
+}
 const resultDisplayArray = makeList(result.success);
 console.log(resultDisplayArray);
 
 const any = {
-    one: ['MB', 'PK', 'AA'],
-    two: ['RC', 'NTR', 'PB'],
-    three: ['SRK', 'Bhai', 'Ameer']
+  one: ["MB", "PK", "AA"],
+  two: ["RC", "NTR", "PB"],
+  three: ["SRK", "Bhai", "Ameer"],
 };
 
 function something(arra) {
-    const displayingArra = [];
-    for (var i = 0; i < arra.length; i++) {
-        displayingArra.push(`<li class="text-no-warning">${arra[i]}</li>`);
-    }
-    return displayingArra;
+  const displayingArra = [];
+  for (var i = 0; i < arra.length; i++) {
+    displayingArra.push(`<li class="text-no-warning">${arra[i]}</li>`);
+  }
+  return displayingArra;
 }
 const displayingArra = something(any.one);
 console.log(displayingArra);
 
 /* Write Concise Object Literal Declarations Using Simple Fields. */
 
-const createPerson = (name,age,gender) => {
-    return {
-        name:name,
-        age:age,
-        gender:gender
-    };
+const createPerson = (name, age, gender) => {
+  return {
+    name: name,
+    age: age,
+    gender: gender,
+  };
 };
-console.log(createPerson('Harvey Spectre',24,'Alpha-Male'));
+console.log(createPerson("Harvey Spectre", 24, "Alpha-Male"));
 
-const creatingSomething = (name,age) => {
-    return {
-        name:name,
-        age:age
-    };
+const creatingSomething = (name, age) => {
+  return {
+    name: name,
+    age: age,
+  };
 };
-console.log(creatingSomething('Mike',28));
+console.log(creatingSomething("Mike", 28));
+/* This can be simplified by this way: */
+
+const creatingPerson = (name, age, gender) => ({ name, age, gender });
+console.log(creatingPerson("Harvey", "25", "Male"));
+
+/* Writing concise declarative functions. */
+
+/* const bicycle = {
+    gear:2,
+    setGear:function(newGear){
+        "use strict";
+        this.gear = newGear;
+    }
+};
+
+bicycle.setGear(3);
+console.log(bicycle.gear); */
+
+/* The above function can be written as the following: */
+
+const bicycle = {
+  gear: 4,
+  setGear(newGear) {
+    "use strict";
+    this.gear = newGear;
+  },
+};
+bicycle.setGear(3);
+console.log(bicycle.gear);
+
+/* Use a Class Syntax to define a constructor function. */
+
+/* var SpaceShuttle = function(targetPlanet){
+    this.targetPlanet = targetPlanet;
+}
+var zeus = new SpaceShuttle('Jupiter');
+console.log(zeus.targetPlanet); */
+
+class SpaceShuttle {
+  constructor(targetPlanet) {
+    this.targetPlanet = targetPlanet;
+  }
+}
+var zeus = new SpaceShuttle("Jupiter");
+console.log(zeus.targetPlanet);
