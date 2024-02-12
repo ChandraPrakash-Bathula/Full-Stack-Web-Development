21c932ccd8530275a28e74fe171352883e01796e
ChandraPrakash-Bathula <chandu.bathula16@gmail>
2024-02-11 20:57:07 -0600

    even or odd

--- Javascript/JSInterviewQuestions.js
+++ Javascript/JSInterviewQuestions.js
@@ -1,5 +1,11 @@
-// Swapping Two Variables :
+/* Swapping Two Variables : */
 
 let a = 1, b = 2;
 [a, b] = [b, a];
 console.log(a, b); // Outputs: 2, 1
+
+/* Checking the number is Even : */
+
+const isEven = num => num % 2 === 0;
+console.log(isEven(4)); // true
+console.log(isEven(3)); // false
