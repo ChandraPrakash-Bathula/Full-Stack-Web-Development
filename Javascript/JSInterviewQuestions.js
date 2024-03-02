6265d296d0a3385f6eb8753e1f1ad1a92a6f3670
ChandraPrakash-Bathula <chandu.bathula16@gmail>
2024-03-01 20:53:15 -0600

    js

--- Javascript/JSInterviewQuestions.js
+++ Javascript/JSInterviewQuestions.js
@@ -565,3 +565,19 @@ const totalElectronicsValue = orders.reduce((accumulator, order) => {
 }, 0);
 
 console.log(totalElectronicsValue);
+
+/* Interview Qestions : */
+
+//Reversing a String 
+
+function reverseString(str) {
+  let reversed = '';
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed += str[i];
+  }
+  return reversed;
+}
+
+const originalString = 'hello';
+const reversedString = reverseString(originalString);
+console.log(reversedString); // Output: 'olleh'
