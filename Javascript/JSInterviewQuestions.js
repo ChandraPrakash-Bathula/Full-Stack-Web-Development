41bc5758e72ba3ca47ae6b2e62cbf27199bd0aa1
ChandraPrakash-Bathula <chandu.bathula16@gmail>
2024-03-07 12:01:09 -0600

    reversing an array

--- Javascript/JSInterviewQuestions.js
+++ Javascript/JSInterviewQuestions.js
@@ -754,3 +754,20 @@ promise.then(
   result => console.log(result), // "done" after 1 second
   error => console.log(error) // doesn't run
 );
+
+/* Reversing an Array : */
+
+function reverseArray(arr) {
+  let start = 0;
+  let end = arr.length - 1;
+  while (start < end) {
+      let temp = arr[start];
+      arr[start] = arr[end];
+      arr[end] = temp;
+      start++;
+      end--;
+  }
+  return arr;
+}
+
+console.log(reverseArray([1, 2, 3, 4, 5])); // Output: [5, 4, 3, 2, 1]
